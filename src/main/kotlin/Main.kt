976748811f4d273e51ade3ad1e98b425bf06d68b
zaidbashir <zaidwani2282@gmail.com>
2023-10-18 19:24:00 +0530

    Initial Commi

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    //Go to More Options i-e Three Dots at Top Right
+    //to Pass Program Arguments that are Stored in Array args
+    println("Program arguments: ${args[0]}")
+}
