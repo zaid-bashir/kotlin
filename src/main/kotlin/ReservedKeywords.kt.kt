c34e3bee969cba5ff8e08efedebfd878ffddea91
zaidbashir <zaidwani2282@gmail.com>
2023-10-18 21:33:22 +0530

    Variables and Data Types

--- src/main/kotlin/ReservedKeywords.kt.kt
+++
@@ -1,5 +0,0 @@
-fun main(){
-    // val is Reserved Keyword, We can't use it for Naming variables or functions
-    val name = "Zaid Bashir"
-    println(name)
-}
---
+++ src/main/kotlin/VariablesAndDataTypes.kt
@@ -0,0 +1,13 @@
+fun main(){
+    // val : Used To Create Variable Which can be Assigned Only Once.
+    // var : Used To Create Variable Which can be Reassigned .
+
+    val userId : String = "CR_76548_2023"
+    var userName : String = "Zaid Bashir"
+    println(userName)
+    userName += " Wani"
+    println(userId)
+    println(userName)
+
+
+}
